a41f3f97601c15163d72c39d35db901a2ead3260
Tucker Timothy <tuckj90@gmail.com>
2026-01-17 13:43:37 -0700

    Implemented ChessBoard constructor, addpiece, and getPiece.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,6 +10,8 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    // The board variable uses indices 0-7 while the ChessPosition class uses values 1-8
+    // This means when assigning to the board variable we must subtract one from our Position to get the right index.
     ChessPiece[][] board;
     public ChessBoard() {
         board = new ChessPiece[8][8];
@@ -19,7 +24,9 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int rowIndex = position.getRow() - 1;
+        int colIndex = position.getColumn() - 1;
+        board[rowIndex][colIndex] = piece;
     }
 
     /**
@@ -30,7 +37,9 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int rowIndex = position.getRow() - 1;
+        int colIndex = position.getColumn() - 1;
+        return board[rowIndex][colIndex];
     }
 
     /**
@@ -39,5 +48,19 @@ public class ChessBoard {
      */
     public void resetBoard() {
          board = new ChessPiece[8][8];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
