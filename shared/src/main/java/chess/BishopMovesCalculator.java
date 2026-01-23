3585e1fa651fee1a214ccf4a92c107d461933b93
Tucker Timothy <tuckj90@gmail.com>
2026-01-22 20:41:29 -0700

    Changed pieceMoves implementation, built calcultor classes. Fixed BishopMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,11 +1,13 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMovesCalculator implements PieceMovesCalculator{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        return moves;
     }
 }
