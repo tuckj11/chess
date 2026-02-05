e39cad15c160a87f3e261601aadb76f7f380eb7e
Tucker Timothy <tuckj90@gmail.com>
2026-02-05 11:28:23 -0700

    Implemented isInStalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -183,7 +183,17 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+        for(int i = 1; i < 9; i++) {
+            for(int j = 1; j < 9; j++) {
+                ChessPosition position = new ChessPosition(i,j);
+                ChessPiece piece = board.getPiece(position);
+                if(piece != null && piece.getTeamColor() == teamColor) {
+                    possibleMoves.addAll(validMoves(position));
+                }
+            }
+        }
+        return possibleMoves.isEmpty() && !isInCheck(teamColor);
     }
 
     /**
