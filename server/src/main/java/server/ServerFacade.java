562db37e9e7af43fa62c852446588f77d76832e9
Tucker Timothy <tuckj90@gmail.com>
2026-03-23 20:54:24 -0600

    Setup Private Methods

--- server/src/main/java/server/ServerFacade.java
+++ server/src/main/java/server/ServerFacade.java
@@ -1,9 +1,15 @@
 package server;
 
+import com.google.gson.Gson;
+import io.javalin.http.HttpResponseException;
 import service.UserService;
 import service.GameService;
 import service.ClearService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -46,15 +52,55 @@ public class ServerFacade {
 
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
-        URL url = (new URI(serverUrl + path)).toURL();
-        HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod(method);
-        http.setDoOutput(true);
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws HttpResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
 
-        writeBody(request, http);
-        http.connect();
-        //throwIfNotSuccessful(http);
-        //return readBody()
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+
+        } catch (Exception e) {
+            throw new HttpResponseException(500, e.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, HttpResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new HttpResponseException(status, "failure: " + status);
+        }
+
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try(InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if(responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
