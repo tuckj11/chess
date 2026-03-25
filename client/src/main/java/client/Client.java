40d0476e227c9564178bac59b276d9ecc253214c
Tucker Timothy <tuckj90@gmail.com>
2026-03-24 18:40:12 -0600

    Setup preLoginLoop

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -14,6 +14,40 @@ public class Client {
     }
 
     public void run() {
+        System.out.println("Welcome to Chess! Let's get started");
+        preLoginLoop();
+    }
+
+    private void preLoginLoop() {
+        System.out.println("Type Help to see your options.");
+        while(true) {
+            String input = scan.nextLine().trim().toLowerCase();
+            switch (input) {
+                case "register" -> register();
+                case "login" -> login();
+                case "quit" -> quit();
+                case "help" -> help();
+                default -> {
+                    System.out.println("Sorry that is an unrecognized command. Please try one of the following");
+                    help();
+                }
+            }
+        }
+    }
+
+    private void register() {
+
+    }
+
+    private void login() {
+
+    }
+
+    private void quit() {
+
+    }
+
+    private void help() {
 
     }
 }
