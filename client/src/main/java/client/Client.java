6fe8e226307271e2442fbc67b8f0a6ca69a32fab
Tucker Timothy <tuckj90@gmail.com>
2026-03-24 18:22:08 -0600

    Setup client classes and built constructors

---
+++ client/src/main/java/client/Client.java
@@ -0,0 +1,15 @@
+package client;
+
+import server.ServerFacade;
+
+import java.util.Scanner;
+
+public class Client {
+    private final ServerFacade serverFacade;
+    private final Scanner scan;
+
+    public Client() {
+        serverFacade = new ServerFacade("http://localhost:8080");
+        scan = new Scanner(System.in);
+    }
+}
--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -4,7 +4,6 @@ import chess.*;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+
     }
 }
